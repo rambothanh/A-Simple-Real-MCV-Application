eb56d50ec6665420f8fd0fcd0eb98949b9075102
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2018-12-02 23:15:04 +0700

    Modify router, modify page link

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -16,16 +16,61 @@ namespace SportsStore.WebUI
 
             //cải thiện Url của từng page vd: ?page=3 -> Page3
             //Lưu ý: hệ thống xử lý các route theo thứ tự từ trên xuống
-            routes.MapRoute(
-                name: null,
-                url: "Page{page}",
-                defaults: new { controller = "Product", action = "List" }
-                );
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
+                null,
+                "",
+                new
+                {
+                    controller = "Product",
+                    action = "List",
+                    category = (string)null,
+                    page = 1
+                }
+            );
+
+            routes.MapRoute(
+                null,
+                "Page{page}",
+                new
+                {
+                    controller = "Product",
+                    action = "List",
+                    category = (string)null,
+                },
+                new { page = @"\d+" }
+            );
+
+
+
+            routes.MapRoute(
+                name: null,
+                url: "{category}",
+                defaults: new
+                {
+                    controller = "Product",
+                    action = "List",
+                    page = 1
+                }
+            );
+
+            routes.MapRoute(
+                null,
+                "{category}/Page{page}",
+                new
+                {
+                    controller = "Product",
+                    action = "List",
+
+                },
+                new { page = @"\d+" }
+            );
+
+
+            routes.MapRoute(
+                name: null,
+                url: "{controller}/{action}"
+
             );
         }
     }
