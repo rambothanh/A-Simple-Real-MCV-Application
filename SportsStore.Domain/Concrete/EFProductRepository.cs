622b7e95e4dcc75f182d7c62f3a851316988d3e9
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2018-12-02 14:45:51 +0700

    add EFProductRepository, change NinjectDependencyResolver

--- SportsStore.Domain/Concrete/EFProductRepository.cs
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.Domain.Concrete
 {
-    class EFProductRepository
+    public class EFProductRepository : IProductRepository
     {
+        private EFDbContext context = new EFDbContext();
+
+        // implements the IProductRepository interface
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+
+        //sử dụng một thể hiện của EFDbContext để lấy dữ liệu từ database
+        //bằng cách sử dụng EF
     }
 }
--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Moq;
 using Ninject;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
@@ -13,36 +14,45 @@ namespace SportsStore.WebUI.Infrastructure
     public class NinjectDependencyResolver : IDependencyResolver
     {
         private IKernel kernel;
+
         public NinjectDependencyResolver(IKernel kernelParam)
         {
             kernel = kernelParam;
             AddBindings();
         }
+
         public object GetService(Type serviceType)
         {
             return kernel.TryGet(serviceType);
         }
+
         public IEnumerable<object> GetServices(Type serviceType)
         {
             return kernel.GetAll(serviceType);
         }
+
         private void AddBindings()
         {
             // put bindings here
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product> {
-                new Product { Name = "Football", Price = 25 },
-                new Product { Name = "Surf board", Price = 179 },
-                new Product { Name = "Running shoes", Price = 95 }
-            });
+            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product> {
+            //    new Product { Name = "Football", Price = 25 },
+            //    new Product { Name = "Surf board", Price = 179 },
+            //    new Product { Name = "Running shoes", Price = 95 }
+            //});
 
-            //Ninject trả về cùng một đối tượng giả bất cứ khi nào
-            //nó nhận được yêu cầu implementation lớp interface IProductRepository
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            ////Ninject trả về cùng một đối tượng giả bất cứ khi nào
+            ////nó nhận được yêu cầu implementation lớp interface IProductRepository
+            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
 
-            //Thay vì tạo một thể hiện mới của đối tượng implementation mỗi lần
-            //,Ninject sẽ luôn đáp ứng các yêu cầu cho interface IProductRepository
-            //với cùng một đối tượng giả lập.
+            ////Thay vì tạo một thể hiện mới của đối tượng implementation mỗi lần
+            ////,Ninject sẽ luôn đáp ứng các yêu cầu cho interface IProductRepository
+            ////với cùng một đối tượng giả lập.
+
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+            //Dòng bên trên cho Ninject tạo ra các cá thể của lớp
+            //EFProductRepository để phục vụ các requests đến interface IProductRepository.
+
         }
     }
 }
